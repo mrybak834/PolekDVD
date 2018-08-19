b616202bac9bb9d4ba9584b277837d638ec0b0dc
Marek <mrybak834@Mareks-Air.lan>
2018-08-18 23:29:22 -0500

    Initial setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import configureStore from "./redux/store/config";
+import AppRouter, { history } from "./routers/Router";
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+const store = configureStore();
+const jsx = (
+  <Provider store={store}>
+      <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
