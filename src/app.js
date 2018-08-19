10abc699220b5364f40f7739934d953180d7e614
Marek <mrybak834@Mareks-Air.lan>
2018-08-19 02:29:53 -0500

    Material UI

--- src/app.js
+++ src/app.js
@@ -3,13 +3,16 @@ import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import configureStore from "./redux/store/config";
 import AppRouter, { history } from "./routers/Router";
-import 'normalize.css/normalize.css';
+import CssBaseline from "@material-ui/core/CssBaseline";
 import './styles/styles.scss';
 
 const store = configureStore();
 const jsx = (
   <Provider store={store}>
+    <React.Fragment>
+      <CssBaseline />
       <AppRouter />
+    </React.Fragment>
   </Provider>
 );
 
