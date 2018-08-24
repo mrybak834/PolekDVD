04cc6f753764a273d217773d186d4c6aa780c723
mrybak834 <mrybak834@gmail.com>
2018-08-23 22:36:13 -0500

    Full load script working

--- src/scripts/loadDB.js
+++ src/scripts/loadDB.js
@@ -27,163 +27,171 @@ function titleCase(str) {
     }
     // Directly return the joined string
     return splitStr.join(' ');
- }
+}
 
-function upload() {
+let apiCalls = async () => {
     let key;
-    let sequence = requestNumber * 2;
-    for (key = sequence; key < sequence + 1; key++){
+    let sequence = requestNumber * 5;
+
+    for (key = sequence; key <= sequence + 4; key++){
         if(key >= limit){
+            console.log("Hit limit: " + key);
             return;
         }
         const movie = movies[keys[key]];
 
-        let query = movie.titleEN.trim()? movie.titleEN.trim() : movie.titlePL.trim();
+        let query = movie.titlePL.trim()? movie.titlePL.trim() : movie.titleEN.trim();
         query = query.replace("/720p.*/g", "");
         query = query.replace("/1080p.*/g", "");
         query = query.replace("/RC.*/g", "");
         query = query.replace("/HDTV.*/g", "");
 
         // Search for the movie
-        fetch("https://api.themoviedb.org/3/search/movie?api_key="+
+        const fetchResult = await fetch("https://api.themoviedb.org/3/search/movie?api_key="+
                 api_key +
                 "&language=pl&query="+
                 query +
                 "&page=1&include_adult=true")
-        .then(res => res.json())
-        .then(
-          (result) => {
-            let fullTitle = "" + movie.titlePL.trim() + "/" + movie.titleEN.trim();
-            fullTitle = titleCase(fullTitle);
+        const result = await fetchResult.json();
 
-            let titlePL = movie.titlePL.trim();
-            titlePL = titlePL.replace("/720p.*/g", "");
-            titlePL = titlePL.replace("/1080p.*/g", "");
-            titlePL = titlePL.replace("/RC.*/g", "");
-            titlePL = titlePL.replace("/HDTV.*/g", "");
-            titlePL = titleCase(titlePL);
+        let fullTitle = "" + movie.titlePL.trim() + "/" + movie.titleEN.trim();
+        fullTitle = titleCase(fullTitle);
 
-            let titleEN = movie.titleEN.trim();
-            titleEN = titleEN.replace("/720p.*/g", "");
-            titleEN = titleEN.replace("/1080p.*/g", "");
-            titleEN = titleEN.replace("/RC.*/g", "");
-            titleEN = titleEN.replace("/HDTV.*/g", "");
-            titleEN = titleCase(titleEN);
+        let titlePL = movie.titlePL.trim();
+        titlePL = titlePL.replace("/720p.*/g", "");
+        titlePL = titlePL.replace("/1080p.*/g", "");
+        titlePL = titlePL.replace("/RC.*/g", "");
+        titlePL = titlePL.replace("/HDTV.*/g", "");
+        titlePL = titleCase(titlePL);
 
-            let link = movie.link.trim();
-            let year = movie.year;
-            let genre = movie.genre.trim();
-            genre = titleCase(genre);
-            let rating = ( () => {
-                switch(movie.rating.trim()) {
-                    case "REWELACYJNY": {
-                        return 9;
-                    }
-                    case "BARDZO DOBRY": {
-                        return 8;
-                    }
-                    case "DOBRY": {
-                        return 7;
-                    }
-                    case "NIEZŁY": {
-                        return 6;
-                    }
-                    case "ŚREDNI": {
-                        return 5;
-                    }
-                    case "UJDZIE": {
-                        return 4;
-                    }
-                    case "SŁABY": {
-                        return 3;
-                    }
-                    default: {
-                        return 5;
-                    }
+        let titleEN = movie.titleEN.trim();
+        titleEN = titleEN.replace("/720p.*/g", "");
+        titleEN = titleEN.replace("/1080p.*/g", "");
+        titleEN = titleEN.replace("/RC.*/g", "");
+        titleEN = titleEN.replace("/HDTV.*/g", "");
+        titleEN = titleCase(titleEN);
+
+        let link = movie.link.trim();
+        let year = movie.year;
+        let genre = movie.genre.trim();
+        genre = titleCase(genre);
+        let rating = ( () => {
+            switch(movie.rating.trim()) {
+                case "REWELACYJNY": {
+                    return 9;
                 }
-            } ) ();
-
-
-
-            if (result.results.length === 0){
-
-                console.log("NOT FOUND: ");
-                console.log(fullTitle);
-                console.log(titlePL);
-                console.log(titleEN);
-                console.log(link);
-                console.log(year);
-                console.log(genre);
-                console.log(rating);
-                console.log();
-                console.log();
-                // Upload to DB
-                // database.ref('movies').update({
-                //     [keys[key]]: {
-                //         "fullTitle": fullTitle,
-                //         "titlePL": titlePL,
-                //         "titleEN": titleEN,
-                //         "link": link,
-                //         "year": year,
-                //         "genre": genre,
-                //         "rating": rating,
-                //         "description": "",
-                //         "posterPath": "",
-                //         "backdropPath": ""
-                //     }
-                // });
+                case "BARDZO DOBRY": {
+                    return 8;
+                }
+                case "DOBRY": {
+                    return 7;
+                }
+                case "NIEZŁY": {
+                    return 6;
+                }
+                case "ŚREDNI": {
+                    return 5;
+                }
+                case "UJDZIE": {
+                    return 4;
+                }
+                case "SŁABY": {
+                    return 3;
+                }
+                default: {
+                    return 5;
+                }
             }
-            else {
-                titlePL = titlePL? titlePL : result.results[0].title;
-                titleEN = titleEN? titleEN : result.results[0].original_title;
-                year = year? year : result.results[0].release_date;
-                genre = genre? genre : (() => {
-                    let genreString = "";
-                    result.results[0].genre_ids.forEach((id) => {
-                        genreString += genres[id].name;
-                    });
-
-                    return genreString;
-                })();
-                rating = result.results[0].vote_average? result.results[0].vote_average : rating;
-                const description = result.results[0].overview;
-                const posterPath = "http://image.tmdb.org/t/p/w500/" + result.results[0].poster_path;
-                const backdropPath = "http://image.tmdb.org/t/p/w500/" + result.results[0].backdrop_path;
+        } ) ();
 
 
-                console.log("FOUND: ");
-                console.log(fullTitle);
-                console.log(titlePL);
-                console.log(titleEN);
-                console.log(link);
-                console.log(year);
-                console.log(genre);
-                console.log(rating);
-                console.log(description);
-                console.log(posterPath);
-                console.log(backdropPath);
-                console.log(result);
 
-                                // // Upload to DB
-                // database.ref('movies').update({
-                //     [keys[key]]: {
-                //         "fullTitle": fullTitle,
-                //         "titlePL": titlePL,
-                //         "titleEN": titleEN,
-                //         "link": link,
-                //         "year": year,
-                //         "genre": genre,
-                //         "rating": rating,
-                //         "description": "",
-                //         "posterPath": "",
-                //         "backdropPath": ""
-                //     }
-                // });
-            }
-          });
+        if (result.results.length === 0){
+
+            console.log("NOT FOUND: ");
+            console.log(fullTitle);
+            // console.log(titlePL);
+            // console.log(titleEN);
+            // console.log(link);
+            // console.log(year);
+            // console.log(genre);
+            // console.log(rating);
+            // console.log();
+            // console.log();
+
+            // Upload to DB
+            database.ref('movies').update({
+                [keys[key]]: {
+                    "fullTitle": fullTitle,
+                    "titlePL": titlePL,
+                    "titleEN": titleEN,
+                    "link": link,
+                    "year": year,
+                    "genre": genre,
+                    "rating": rating,
+                    "description": "",
+                    "posterPath": "",
+                    "backdropPath": ""
+                }
+            });
+        }
+        else {
+            titlePL = result.results[0].title;
+            titleEN = result.results[0].original_title;
+            year = year? year : result.results[0].release_date;
+            genre = genre? genre : (() => {
+                let genreString = "";
+                result.results[0].genre_ids.forEach((id) => {
+                    genreString += genres[id].name;
+                });
+
+                return genreString;
+            })();
+            rating = result.results[0].vote_average? result.results[0].vote_average : rating;
+            const description = result.results[0].overview;
+            const posterPath = "http://image.tmdb.org/t/p/w500/" + result.results[0].poster_path;
+            const backdropPath = "http://image.tmdb.org/t/p/w500/" + result.results[0].backdrop_path;
+
+
+            // console.log("FOUND: ");
+            // console.log(fullTitle);
+            // console.log(titlePL);
+            // console.log(titleEN);
+            // console.log(link);
+            // console.log(year);
+            // console.log(genre);
+            // console.log(rating);
+            // console.log(description);
+            // console.log(posterPath);
+            // console.log(backdropPath);
+            // console.log(result);
+
+            // Upload to DB
+            database.ref('movies').update({
+                [keys[key]]: {
+                    "fullTitle": fullTitle,
+                    "titlePL": titlePL,
+                    "titleEN": titleEN,
+                    "link": link,
+                    "year": year,
+                    "genre": genre,
+                    "rating": rating,
+                    "description": description,
+                    "posterPath": posterPath,
+                    "backdropPath": backdropPath
+                }
+            });
+        }
     }
 
     requestCallback();
+}
+
+
+
+
+function upload() {
+    apiCalls();
 };
 
 
@@ -201,9 +209,9 @@ function requestCallback() {
     console.log(requestNumber);
     endTime = new Date();
     var diff = timeDiff();
-    if(diff < 12000) {
+    if(diff < 5000) {
         //Too early to start API, need to wait.
-        setTimeout(startRequest, 12000 - diff);
+        setTimeout(startRequest, 5000 - diff);
     } else {
         //It is fine to start request now.
         setTimeout(startRequest);
