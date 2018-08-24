d1ecf8145d8c8f0817c202721481a923e64d7e8e
mrybak834 <mrybak834@gmail.com>
2018-08-24 17:06:22 -0500

    Converted grid to class based component

--- src/components/ui/Grid.js
+++ src/components/ui/Grid.js
@@ -9,14 +9,10 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import GridUI from '@material-ui/core/Grid';
 import { withStyles } from '@material-ui/core/styles';
-
+import database from '../../firebase/firebase';
 
 const styles = theme => ({
     layout: {
-        // flex: 1,
-        // width: '100%',
-        // maxHeight: '100%',
-        // backgroundColor: theme.palette.background.paper,
         width: 'auto',
         marginLeft: theme.spacing.unit * 3,
         marginRight: theme.spacing.unit * 3,
@@ -42,22 +38,35 @@ const styles = theme => ({
       },
 });
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 
-function Grid(props) {
-  const { classes } = props;
-  return (
-    <div className={classNames(classes.layout, classes.cardGrid)}>
+// TODO
+// Make this a stateful class component
+// Do ajax calls in componentwillmount
+// set up default blank cards in state before ajax call modifies the state
+
+class Grid extends React.Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      cards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    }
+  }
+
+  render() {
+    return (
+      <div className={classNames(this.props.classes.layout, this.props.classes.cardGrid)}>
         <GridUI container spacing={40}>
-            {cards.map(card => (
+            {this.state.cards.map(card => (
             <GridUI item key={card} sm={6} md={4} lg={3}>
-                <Card className={classes.card}>
+                <Card className={this.props.classes.card}>
                 <CardMedia
-                    className={classes.cardMedia}
+                    className={this.props.classes.cardMedia}
                     image="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22288%22%20height%3D%22225%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20288%20225%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_164edaf95ee%20text%20%7B%20fill%3A%23eceeef%3Bfont-weight%3Abold%3Bfont-family%3AArial%2C%20Helvetica%2C%20Open%20Sans%2C%20sans-serif%2C%20monospace%3Bfont-size%3A14pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_164edaf95ee%22%3E%3Crect%20width%3D%22288%22%20height%3D%22225%22%20fill%3D%22%2355595c%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%2296.32500076293945%22%20y%3D%22118.8%22%3EThumbnail%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E" // eslint-disable-line max-len
                     title="Image title"
                 />
-                <CardContent className={classes.cardContent}>
+                <CardContent className={this.props.classes.cardContent}>
                     <Typography gutterBottom variant="headline" component="h2">
                     Heading
                     </Typography>
@@ -77,8 +86,9 @@ function Grid(props) {
             </GridUI>
             ))}
         </GridUI>
-    </div>
-  );
+      </div>
+    )
+  }
 }
 
 Grid.propTypes = {
